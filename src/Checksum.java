93fa42cd29b2a9e22cf534f5ad7ac25a898f6495
heckler82 <heckler82@gmail.com>
2018-12-02 01:16:33 -0500

    Removed unneeded imports

--- src/Checksum.java
+++ src/Checksum.java
@@ -1,9 +1,5 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Scanner;
 
 /**
  * Solution for Day02 of Advent of Code
