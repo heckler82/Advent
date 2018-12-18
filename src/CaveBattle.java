0a918e186c3ad300219496b278dabc22c37b7821
heckler82 <heckler82@gmail.com>
2018-12-17 21:49:42 -0500

    Re-organized some of the code in CaveBattle.java

--- src/CaveBattle.java
+++ src/CaveBattle.java
@@ -13,6 +13,8 @@ public class CaveBattle extends AdventMaster {
     private int elfCount = 0;
     private int goblinCount = 0;
     private int elfDamage = 23;
+    private int goblinTeam = 1;
+    private int elfTeam = 2;
 
     /**
      * Creates a new instance
@@ -38,28 +40,14 @@ public class CaveBattle extends AdventMaster {
         int round = 0;
         // Print out the initial state
         System.out.println("Initial State");
-        for(int g = 0; g < map.length; g++) {
-            for(int h = 0; h < map[g].length; h++) {
-                if(map[g][h] == 1)
-                    System.out.print('.');
-                else
-                    System.out.print(map[g][h]);
-            }
-            for(GameObject obj : objects) {
-                if(obj.y == g) {
-                    System.out.printf("\t%c(%d)", obj.team == 2 ? 'G' : 'E',obj.health);
-                }
-            }
-            System.out.println();
-        }
-        System.out.println();
+        printMap(map);
         // Get the initial goblin and elf counts
         int initialGoblins = goblinCount;
         int initialElves = elfCount;
         // Continue to wage war as long as both races still have soldiers to fight
-        while(elfCount > 0 && goblinCount >0) {
+        while(elfCount > 0 && goblinCount > 0) {
             boolean roundCompleted = true;
-            // Sort objects
+            // Preserve read order for turn priority
             Collections.sort(objects);
             for(GameObject obj : objects) {
                 // If object is dead ignore it
@@ -71,69 +59,30 @@ public class CaveBattle extends AdventMaster {
                 if(!targets.isEmpty()) {
                     // If there is a target in range, attack it
                     List<GameObject> targetsInRange = getInRangeTargets(targets, obj);
-                    Collections.sort(targetsInRange);
                     if(targetsInRange.isEmpty()) {
-                        // Preserve read order
-                        Collections.sort(targets);
                         // Begin move ops
                         // Find all open squares around targets (No open squares are on the edge on the map)
                         List<Point> openSquares = findOpenSquares(map, targets, obj);
                         if(!openSquares.isEmpty()) {
                             // Save squares that are reachable (Pathfinding)
-                            Dijkstra d = new Dijkstra(map, openSquares, obj.x, obj.y);
-                            List<Point> nearest = d.nearest;
+                            Dijkstra dijkstra = new Dijkstra(map, openSquares, obj.x, obj.y);
                             // This is the point we want to move to
-                            if(!nearest.isEmpty()) {
-                                Point moveTo = d.constructPath(nearest.get(0), obj.x, obj.y);
+                            if(!dijkstra.nearest.isEmpty()) {
+                                Point moveTo = dijkstra.constructPath(dijkstra.nearest.get(0), obj.x, obj.y);
                                 obj.move(map, moveTo.x - obj.x, moveTo.y - obj.y);
                                 // If in range of target, attack (select in range target utilizing read order)
                                 // Identify all remaining objects of opposing team
                                 targets = findTargets(objects, obj.team == 1 ? 2 : 1);
                                 if (!targets.isEmpty()) {
                                     targetsInRange = getInRangeTargets(targets, obj);
-                                    Collections.sort(targetsInRange);
                                     if (!targetsInRange.isEmpty()) {
-                                        // Select target with lowest remaining health (read order is already preserved)
-                                        GameObject target = null;
-                                        int minHealth = Integer.MAX_VALUE;
-                                        for (GameObject potentialTarget : targetsInRange) {
-                                            if (potentialTarget.health < minHealth) {
-                                                minHealth = potentialTarget.health;
-                                                target = potentialTarget;
-                                            }
-                                        }
-                                        // Attack here; if target dies, decrement count for its team
-                                        if (!target.damage(obj.damage)) {
-                                            map[target.y][target.x] = 1;
-                                            if (target.team == 1) {
-                                                goblinCount--;
-                                            } else {
-                                                elfCount--;
-                                            }
-                                        }
+                                        attackTargetInRange(targetsInRange, obj, map);
                                     }
                                 }
                             }
                         }
                     } else {
-                        // Select target with lowest remaining health (read order is already preserved)
-                        GameObject target = null;
-                        int minHealth = Integer.MAX_VALUE;
-                        for(GameObject potentialTarget : targetsInRange) {
-                            if(potentialTarget.health < minHealth) {
-                                minHealth = potentialTarget.health;
-                                target = potentialTarget;
-                            }
-                        }
-                        // Attack here; if target dies, decrement count for its team
-                        if(!target.damage(obj.damage)) {
-                            map[target.y][target.x] = 1;
-                            if(target.team == 1) {
-                                goblinCount--;
-                            } else {
-                                elfCount--;
-                            }
-                        }
+                        attackTargetInRange(targetsInRange, obj, map);
                     }
                 } else {
                     // No targets remaining, early exit
@@ -141,44 +90,23 @@ public class CaveBattle extends AdventMaster {
                     break;
                 }
             }
+            // Remove dead objects
+            for(int i = objects.size() - 1; i >= 0; i--) {
+                GameObject obj = objects.get(i);
+                if(!obj.isAlive) {
+                    objects.remove(i);
+                }
+            }
             // Complete round
             if(roundCompleted) {
                 round++;
             }
             System.out.println("Round " + round);
-            for(int g = 0; g < map.length; g++) {
-                for(int h = 0; h < map[g].length; h++) {
-                    if(map[g][h] == 1)
-                        System.out.print('.');
-                    else
-                        System.out.print(map[g][h]);
-                }
-                for(GameObject obj : objects) {
-                    if(obj.y == g && obj.isAlive) {
-                        System.out.printf("\t%c(%d)", obj.team == 2 ? 'G' : 'E',obj.health);
-                    }
-                }
-                System.out.println();
-            }
-            System.out.println();
+            printMap(map);
         }
         // Get the total hit points from the remaining objects
         System.out.println("Final round: " + round);
-        for(int g = 0; g < map.length; g++) {
-            for(int h = 0; h < map[g].length; h++) {
-                if(map[g][h] == 1)
-                    System.out.print('.');
-                else
-                    System.out.print(map[g][h]);
-            }
-            for(GameObject obj : objects) {
-                if(obj.y == g && obj.isAlive) {
-                    System.out.printf("\t%c(%d)", obj.team == 2 ? 'G' : 'E',obj.health);
-                }
-            }
-            System.out.println();
-        }
-        System.out.println();
+        printMap(map);
         int answer = 0;
         for(GameObject object : objects) {
             if(object.isAlive) {
@@ -190,6 +118,69 @@ public class CaveBattle extends AdventMaster {
         System.out.printf("The goblins have %d / %d soldiers remaining%n", goblinCount, initialGoblins);
         // Get the completed rounds multiplied by the total remaining health
         return answer * round;
+    }
+
+    /**
+     * Attacks a target that is in range
+     *
+     * @param targetsInRange The targets in range
+     * @param obj The attacking object
+     * @param map The map
+     */
+    private void attackTargetInRange(List<GameObject> targetsInRange, GameObject obj, int[][] map) {
+        // Select target with lowest remaining health (read order is already preserved)
+        GameObject target = null;
+        int minHealth = Integer.MAX_VALUE;
+        for (GameObject potentialTarget : targetsInRange) {
+            if (potentialTarget.health < minHealth) {
+                minHealth = potentialTarget.health;
+                target = potentialTarget;
+            }
+        }
+        // Attack here; if target dies, decrement count for its team
+        if (!target.damage(obj.damage)) {
+            map[target.y][target.x] = 1;
+            if (target.team == 1) {
+                goblinCount--;
+            } else {
+                elfCount--;
+            }
+        }
+    }
+
+    /**
+     * Prints out the state of the map
+     *
+     * @param map The map
+     */
+    private void printMap(int[][] map) {
+        for(int i = 0; i < map.length; i++) {
+            for(int j = 0; j < map[i].length; j++) {
+                // If open space, print blank
+                int space = map[i][j];
+                char print = '#';
+                switch(space) {
+                    case 1:
+                        print = ' ';
+                        break;
+                    case 2:
+                        print = 'G';
+                        break;
+                    case 3:
+                        print = 'E';
+                        break;
+                }
+                System.out.print(print);
+            }
+            // Print current object stats
+            for(GameObject obj : objects) {
+                if(obj.y == i && obj.isAlive) {
+                    System.out.printf("\t%c(%d)", obj.team == 2 ? 'E' : 'G',obj.health);
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 
     /**
@@ -207,6 +198,8 @@ public class CaveBattle extends AdventMaster {
                 list.add(obj);
             }
         }
+        // Preserve read order
+        Collections.sort(list);
         return list;
     }
 
@@ -256,11 +249,14 @@ public class CaveBattle extends AdventMaster {
      */
     private List<GameObject> findTargets(List<GameObject> list, int team) {
         ArrayList<GameObject> targets = new ArrayList<>();
+        // Get all live members of the other team
         for(GameObject obj : list) {
             if(obj.team == team && obj.isAlive) {
                 targets.add(obj);
             }
         }
+        // Preserve read order
+        Collections.sort(targets);
         return targets;
     }
 
@@ -294,14 +290,14 @@ public class CaveBattle extends AdventMaster {
                     case 'G':
                         // Open space, but a goblin occupies it
                         strip[j] = 2;
-                        GameObject goblin = new GameObject(j, i, 200, 1, 3);
+                        GameObject goblin = new GameObject(j, i, 200, goblinTeam, 3);
                         objects.add(goblin);
                         goblinCount++;
                         break;
                     case 'E':
                         // Open space, but an elf occupies it
                         strip[j] = 3;
-                        GameObject elf = new GameObject(j, i, 200, 2, elfDamage);
+                        GameObject elf = new GameObject(j, i, 200, elfTeam, elfDamage);
                         objects.add(elf);
                         elfCount++;
                 }
@@ -313,6 +309,9 @@ public class CaveBattle extends AdventMaster {
         return map;
     }
 
+    /**
+     * Game object
+     */
     private class GameObject implements Comparable<GameObject> {
         int x = 0;
         int y = 0;
@@ -321,6 +320,15 @@ public class CaveBattle extends AdventMaster {
         boolean isAlive = true;
         public int team = 1;
 
+        /**
+         * Creates a new game object
+         *
+         * @param x The x coordinate
+         * @param y The y coordinate
+         * @param health The health
+         * @param team The team
+         * @param damage The damage the object inflicts
+         */
         public GameObject(int x, int y, int health, int team, int damage) {
             this.x = x;
             this.y = y;
@@ -329,6 +337,13 @@ public class CaveBattle extends AdventMaster {
             this.damage = damage;
         }
 
+        /**
+         * Moves the object
+         *
+         * @param map The map
+         * @param dx The y distance to move
+         * @param dy The x distance to move
+         */
         public void move(int[][] map, int dx, int dy) {
             // Open the current space
             map[y][x] = 1;
@@ -339,6 +354,12 @@ public class CaveBattle extends AdventMaster {
             map[y][x] = 1 + team;
         }
 
+        /**
+         * Damages the object
+         *
+         * @param damage The amount of damage taken
+         * @return True if the object is still alive
+         */
         public boolean damage(int damage) {
             health -= damage;
             if(health <= 0) {
@@ -347,6 +368,12 @@ public class CaveBattle extends AdventMaster {
             return isAlive;
         }
 
+        /**
+         * Compares this game object to another preserving read order
+         *
+         * @param object The game object to compare with
+         * @return The resulting comparison
+         */
         public int compareTo(GameObject object) {
             int comp = Integer.compare(y, object.y);
             if(comp < 0) {
@@ -359,10 +386,21 @@ public class CaveBattle extends AdventMaster {
         }
     }
 
+    /**
+     * Pathfinding class
+     */
     private class Dijkstra {
         ArrayList<Point> nearest;
         Node[][] grid;
 
+        /**
+         * Finds the shortest path to all target nodes from a source
+         *
+         * @param map The map
+         * @param targets The target locations
+         * @param sourceX The source x coordinate
+         * @param sourceY The source y coordinate
+         */
         public Dijkstra(int[][] map, List<Point> targets, int sourceX, int sourceY) {
             grid = new Node[map.length][map[0].length];
             // Fill the Node array
@@ -421,7 +459,7 @@ public class CaveBattle extends AdventMaster {
                     if(newScore == n.score) {
                         Node prevNode = n.prev;
                         PointComparator c = new PointComparator();
-                        //Point p2 = new Point(n.x, n.y);
+                        // Preserve read order between the current node and the neighbor's previous node
                         Point p2 = new Point(current.x, current.y);
                         Point p1 = new Point(prevNode.x, prevNode.y);
                         if(c.compare(p2, p1) == -1) {
@@ -461,6 +499,14 @@ public class CaveBattle extends AdventMaster {
             Collections.sort(nearest, new PointComparator());
         }
 
+        /**
+         * Reconstructs the path from the source to the goal
+         *
+         * @param p The destination
+         * @param px The source x coordinate
+         * @param py The source y coordinate
+         * @return The point to move to along the path
+         */
         public Point constructPath(Point p, int px, int py) {
             Node current = grid[p.y][p.x];
             while(current.prev != grid[py][px]) {
@@ -470,18 +516,34 @@ public class CaveBattle extends AdventMaster {
         }
     }
 
+    /**
+     * Pathfinding node
+     */
     private class Node implements Comparable<Node> {
         int x;
         int y;
         int score;
         Node prev;
 
+        /**
+         * Creates a new node
+         *
+         * @param x The x coordinate
+         * @param y The y coordinate
+         * @param score The score
+         */
         public Node(int x, int y, int score) {
             this.x = x;
             this.y = y;
             this.score = score;
         }
 
+        /**
+         * Compares this node to another node preserving order
+         *
+         * @param n The other node
+         * @return The resulting comparison
+         */
         public int compareTo(Node n) {
             if(score < n.score) {
                 return -1;
@@ -493,6 +555,9 @@ public class CaveBattle extends AdventMaster {
         }
     }
 
+    /**
+     * Sorts Points in read order
+     */
     private class PointComparator implements Comparator<Point> {
         public int compare(Point p, Point p2) {
             int comp = Integer.compare(p.y, p2.y);
